ce84f714fa8fffccd6a5109a7a4271b83cc79909
cis101 <cis101@secs.iub.edu.bd>
2019-11-06 09:04:13 +0600

    npm, nodeman and second html added

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 var http = require('http');
-var fs = require('fs')
-  var server = http.createServer(function(req, res){
-    
-    fs.readFile('index.html', function(err, data){
-        if(err){
-            res.statusCode = 404;
-            res.setHeader('Content-Type', 'text/plain');
-            res.end('File not Found')
-        }
-        res.setHeader('Content-Type', 'text/html');
-        res.statusCode = 200;
-        res.end(data)
-    });
-  });
+var express = require('express');
+var app = express();
+var server = http.Server(app);
+
+app.get('/', function(request, response){
+  response.sendFile(__dirname + '/index.html')
+
+})
+
+app.get('/second', function(request, response){
+  response.sendFile(__dirname + '/second.html')
+
+})
+  
   server.listen(3000, 'localhost', function(){
     console.log('Server running');
   });
