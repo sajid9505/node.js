ced60f0a02a5217d545f5ab6b2458844f1f46310
cis101 <cis101@secs.iub.edu.bd>
2019-11-06 09:39:36 +0600

    classwork 8c

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ var http = require('http');
 var express = require('express');
 var app = express();
 var server = http.Server(app);
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.post('/new_article', function(request, response){
+  
+  console.log(request.body)
+  response.json({msg: "Successfully received"})
+
+})
 
 app.get('/', function(request, response){
   response.sendFile(__dirname + '/index.html')
