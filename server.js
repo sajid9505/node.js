626af5726caf129862fb3b93468f3c14a9e0a777
cis101 <cis101@secs.iub.edu.bd>
2019-11-11 09:34:27 +0600

    classwork 8d

--- server.js
+++ server.js
@@ -3,30 +3,51 @@ var express = require('express');
 var app = express();
 var server = http.Server(app);
 var bodyParser = require('body-parser')
-
 app.use(bodyParser.urlencoded({extended:true}))
 
-app.post('/new_article', function(request, response){
-  
-  console.log(request.body)
-  response.json({msg: "Successfully received"})
+let articles= []
+
+app.post('/submit', function(request,response){
+  articles.push(request.body)
+  console.log(articles)
+  response.json({msg: "successfully received"})
+})
+
+app.get('/article/:index', function(request, response){
+  if(articles[request.params.index]){
+      response.render('article.ejs', {article:articles[request.params.index]})
+  }else{
+    response.json({msg:"Article not available"})
+  }
 
 })
 
+
+
+// app.post('/new_article', function(request, response){
+  
+//   console.log(request.body)
+//   response.json({msg: "Successfully received"})
+
+// })
+
 app.get('/', function(request, response){
-  response.sendFile(__dirname + '/index.html')
+  response.sendFile(__dirname + '/views/index.html')
 
 })
 
 app.get('/second', function(request, response){
-  response.sendFile(__dirname + '/second.html')
+  response.sendFile(__dirname + '/views/second.html')
 
 })
 
 app.get('/new', function(request, response){
-  response.sendFile(__dirname + '/form.html')
+  response.sendFile(__dirname + '/views/form.html')
 
 })
+
+
+
   
   server.listen(3000, 'localhost', function(){
     console.log('Server running');
