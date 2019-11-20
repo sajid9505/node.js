66f7c6723183dd5ecbebbe082bb8888ce4d982ab
cis101 <cis101@secs.iub.edu.bd>
2019-11-20 08:29:32 +0600

    classwork 9B

--- server.js
+++ server.js
@@ -5,74 +5,94 @@ var server = http.Server(app);
 var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended:true}))
 
-var mongo = require('mongodb')
+// var mongo = require('mongodb')
+
+var mongoose = require('mongoose')
 
 var db, uri = "mongodb+srv://sajid9505:12345@cluster0-ebrcf.mongodb.net/test?retryWrites=true&w=majority"
 
-mongo.MongoClient.connect(uri, 
-  {useNewUrlParser: true, useUnifiedTopology: true},
-  function(err, client){
-    if(err){
-      console.log('Could not connect to MongoDB')
-    }else{
-      db = client.db('node-cw9')
-    }
+mongoose.connect(uri,
+  {useNewUrlParser:true, useUnifiedTopology: true})
+mongoose.connection.on('error', function(err){
+    console.log('Could not connect to mongoDB')
   })
+// mongo.MongoClient.connect(uri, 
+//   {useNewUrlParser: true, useUnifiedTopology: true},
+//   function(err, client){
+//     if(err){
+//       console.log('Could not connect to MongoDB')
+//     }else{
+//       db = client.db('node-cw9')
+//     }
+//   })
 
-  var save = function(form_data){
-    db.createCollection('articles', function(err, collection){
-      var collection = db.collection('articles')
-      collection.save(form_data)
-    })
+  // var save = function(form_data){
+  //   db.createCollection('articles', function(err, collection){
+  //     var collection = db.collection('articles')
+  //     collection.save(form_data)
+  //   })
 
-
+  var Schema = mongoose.Schema
+var articleSchema = new Schema(
+  {
+    title: {
+      type:String,
+      required: "Title is required"
+    },
+    content:{
+      type:String,
+      required: "Content is required"
+    }
   }
+)
+
+var Article = mongoose.model('Article', articleSchema)
+
+app.use(bodyParser.urlencoded({extended:true}))
 
 let articles= []
 
-app.post('/submit', function(request,response){
-  save(request.body)
-  articles.push(request.body)
-  console.log(articles)
-  response.json({msg: "successfully received"})
+app.post('/submit',function(request,response){
+  //save(request.body)
+  let article = new Article(request.body)
+  article.save(function(err,data){
+    if(err){
+          response.status(400).json({msg:"All fiels are require"})
+    }
+    response.status(200).json({article:data})
+  })
+  //articles.push(request.body)
+  //console.log(articles)
+ 
+}) 
+
+app.get('/article/:index',function(request,response){
+ if(request.params.index){
+   //response.json(articles[request.params])
+   //response.render('article.ejs',{article:articles[request.params.index]})
+   Article.find({'_id': request.params.index},function(err,data){
+     if(err){
+       console.log(err)
+       return response.status(400).json({msg:'could not not query the id'})
+
+     }
+     return response.render('article.ejs',{article:data[0]})
+   })
+ }
+ else{
+   return response.json({msg:"Article not found"})
+ }
 })
 
-app.get('/article/:index', function(request, response){
-  if(articles[request.params.index]){
-      response.render('article.ejs', {article:articles[request.params.index]})
-  }else{
-    response.json({msg:"Article not available"})
-  }
-
+app.get('/',function(request,response){
+  response.sendFile(__dirname+'/views/index.html')
 })
-
-
-
-// app.post('/new_article', function(request, response){
-  
-//   console.log(request.body)
-//   response.json({msg: "Successfully received"})
-
-// })
-
-app.get('/', function(request, response){
-  response.sendFile(__dirname + '/views/index.html')
-
+app.get('/second',function(request,response){
+  response.sendFile(__dirname+'/views/second.html')
 })
-
-app.get('/second', function(request, response){
-  response.sendFile(__dirname + '/views/second.html')
-
+app.get('/new',function(request,response){
+  response.sendFile(__dirname+'/views/form.html')
 })
-
-app.get('/new', function(request, response){
-  response.sendFile(__dirname + '/views/form.html')
-
-})
-
-
-
-  
   server.listen(3000, 'localhost', function(){
     console.log('Server running');
   });
